89736233c65bd67b002db37961719e766fb31913
Christopher Rowe <chris.rowe19@outlook.com>
2024-11-29 02:16:52 +0000

    Fixes the licensing preventing pip installation.

--- src/astro_sph_tools/_scripts/ast.py
+++ src/astro_sph_tools/_scripts/ast.py
@@ -1,3 +1,10 @@
 # SPDX-FileCopyrightText: 2024-present Christopher Rowe <chris.rowe19@outlook.com>
 #
 # SPDX-License-Identifier: LicenseRef-NotYetLicensed
+
+#import astro_sph_tools as ast
+
+from QuasarCode import Console
+
+def main() -> None:
+    Console.print_info("Hello world!")
